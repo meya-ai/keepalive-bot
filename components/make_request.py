f37b2a22c6fd0f7faf46f2d98cbc25f3fc644f38
Erik Kalviainen <erik@meya.ai>
2016-08-26 22:14:59 -0400

    Convert to up/down check.

--- components/make_request.py
+++ components/make_request.py
@@ -6,6 +6,16 @@ from meya import Component
 class MakeRequest(Component):
 
     def start(self):
-        response = requests.get(self.db.flow.get('url'))
-        print response.status_code
+        try:
+            response = requests.get(self.db.flow.get('url'), timeout=10)
+        except:
+            response = None
+
+        is_up = "⬇ Down"
+        if response:
+            if response.status_code >= 200 and response.status_code < 300:
+                is_up = "⬆ Up"
+
+        self.db.bot.set('is_up', is_up)
+            
         return self.respond(action="next")
